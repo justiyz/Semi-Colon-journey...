da6c2598dca2541bb97c2dc8e1f2f07af3fb61f6
Inyang Ernest <66737035+justiyz@users.noreply.github.com>
2020-07-15 11:59:52 +0100

    Add files via upload

---
+++ KnightsTour.java
@@ -0,0 +1,57 @@
+package com.account;
+
+
+
+	public class KnightsTour {
+	    private int[][] board = new int[8][8];
+	    private int currentRow, currentCol, counter = 2;
+
+	    public KnightsTour(){
+	        board[0][0] =1;
+	    }
+
+	    public void showBoard(){
+	        for(int row = 0; row<board.length; row++){
+	            for (int col = 0; col<board[row].length; col++){
+	                System.out.print(board[row][col]+ "  ");
+	            }
+	            System.out.println();
+	        }
+	    }
+
+	    public boolean moveKnight(int[] horizontal, int[] vertical){
+	       
+	        for(int moveNumber = 0; moveNumber<8; moveNumber++){
+
+	            currentCol = currentCol + horizontal[moveNumber];
+	            currentRow = currentRow + vertical[moveNumber];
+	            if(validate() && checkForNull()){
+	                board[currentRow][currentCol] = counter;
+	                counter++;
+	            }
+	            else{
+	                return false;
+	            }
+
+//	            System.out.println(currentCol+ " " +currentRow);
+	        }
+	        return false;
+	    }
+
+	    public boolean validate(){
+	        if((currentCol >=0 && currentCol<= 7)&& (currentRow >= 0 && currentRow <=7)){
+	            return true;
+	        }
+	        return false;
+	    }
+
+	    public boolean checkForNull(){
+	        if(board[currentRow][currentCol] == 0){
+	            return true;
+	        }
+	        return false;
+	    }
+	}
+
+
+
---
+++ KnightsTourTest.java
@@ -0,0 +1,39 @@
+package com.account;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class KnightsTourTest {
+	KnightsTour knight;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		knight = new KnightsTour();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testKnight() {
+		int [] horizontal = {2,2,1,-2,1,1,2,-1};
+		int [] vertical =   {1,-1,2,1,-2,1,2};
+		knight.moveKnight(horizontal, vertical);
+		knight.showBoard();
+	}
+
+}
